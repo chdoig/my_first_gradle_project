80a20699e4f6876759d8e9973d800d29453e0a08
Christine Doig <christine.doig.cardet@gmail.com>
2014-12-11 00:37:01 -0600

    first gradle project tutorial

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+
+import com.google.common.collect.Lists;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Lists.newArrayList();
+        System.out.println("Hello, World");
+    }
+
+}
